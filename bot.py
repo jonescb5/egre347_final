c2cc3271bc6f16e230458b95964b0f88c5b57910
chrispy <lesaucier@protonmail.com>
2020-04-21 22:50:35 -0400

    opencv arrays captured using continuous capture video.

--- bot.py
+++ bot.py
@@ -3,31 +3,28 @@ import picamera
 import picamera.array
 import cv2
 
-x_res = 1640
-y_res = 922
-fps = 40
-
 cam = picamera.PiCamera()
 # create references to the capture array and capture stream
-cap = picamera.array.PiRGBArray(cam, size=(x_res, y_res))
-cap_stream = cam.capture_continuous(cap, format="bgr", use_video_port=True)
+# cam.start_preview()
+cap = picamera.array.PiRGBArray(cam)
+
 # wait for camera to warm up
 time.sleep(2.0)
 
 # DEBUG ITR
 itr = 0
 
-for frame in cap_stream:
+for frame in cam.capture_continuous(cap, format='bgr', use_video_port=True):
 
     image = frame.array  # capture a frame
     # do something with it
-
+    print("frame got")
     # TESTING STUFF
-    cv2.imwrite('cap' + itr + '.jpg', image)
+    cv2.imwrite('cap' + str(itr) + '.jpg', image)
 
     cap.truncate(0)  # reset capture array size to zero. effectively clears the array
 
-    if itr == 1:
-        break
+    if itr == 5:
+        exit()
 
-    ++itr
+    itr += 1
