f143f8099fbd6ce7df00ef202161fe0ac6943ce1
chrispy <lesaucier@protonmail.com>
2020-04-21 21:01:07 -0400

    testing out camera capture with pi as remote interpreter for main machine. ssh via wifi also working

--- README.txt
+++ README.txt
@@ -6,6 +6,9 @@ https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 opencv-python documentation
 https://opencv-python-tutroals.readthedocs.io/en/latest/index.html
 
+picamera documentation
+https://picamera.readthedocs.io/en/release-1.13/index.html
+
 for reference to this dude's stupid tools library. just give me the fucking source in the example
 https://github.com/jrosebr1/imutils
 
--- bot.py
+++ bot.py
@@ -3,13 +3,34 @@ import picamera
 import picamera.array
 import cv2
 
-with picamera as camera:
-    # start camera
-    camera.start_preview()
-    # wait for the camera to get going
-    time.sleep(2.0)
+x_res = 1640
+y_res = 922
+fps = 40
 
-    while True:
-        # get frame from camera
+cam = picamera.PiCamera()
+# set camera parameters
+cam.resolution(x_res, y_res)
+cam.framerate = fps
+# create references to the capture array and capture stream
+cap = picamera.array.PiRGBArray(cam, size=(x_res, y_res))
+cap_stream = cam.capture_continuous(cap, format="bgr", use_video_port=True)
+# wait for camera to warm up
+time.sleep(2.0)
 
-        #
+# DEBUG ITR
+itr = 0
+
+for frame in cap_stream:
+
+    image = frame.array  # capture a frame
+    # do something with it
+
+    # TESTING STUFF
+    cv2.imwrite('cap' + itr + '.jpg', image)
+
+    cap.truncate(0)  # reset capture array size to zero. effectively clears the array
+
+    if itr == 1:
+        break
+
+    ++itr
