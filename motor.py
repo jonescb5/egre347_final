7bddee190ec5a37d18f49fc3b737a8ba52df34b1
chrispy <lesaucier@protonmail.com>
2020-04-26 21:07:44 -0400

    motor moving the bot foreward

--- motor.py
+++ motor.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as gpio
+import time
 
 ena = 12
 in1 = 5
@@ -8,7 +9,7 @@ in3 = 20
 in4 = 21
 PWMf = 1000
 
-gpio.setmode(gpio.BOARD)
+gpio.setmode(gpio.BCM)
 gpio.setup(ena, gpio.OUT)
 gpio.setup(in1, gpio.OUT)
 gpio.setup(in2, gpio.OUT)
@@ -18,11 +19,18 @@ gpio.setup(in4, gpio.OUT)
 spa = gpio.PWM(ena, PWMf)
 spb = gpio.PWM(enb, PWMf)
 
+time.sleep(3)
 
-gpio.output(in1, gpio.HIGH)
-gpio.output(in2, gpio.LOW)
+gpio.output(in1, gpio.LOW)
+gpio.output(in2, gpio.HIGH)
 gpio.output(in3, gpio.HIGH)
 gpio.output(in4, gpio.LOW)
 
-spa.start(25)
-spb.start(25)
+spa.start(100)
+spb.start(100)
+
+for x in range (0, 5):
+    time.sleep(1)
+
+gpio.cleanup()
+print("run")
